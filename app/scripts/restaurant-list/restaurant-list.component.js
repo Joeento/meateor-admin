dc93b1618c960863b26a2f5718df2375237c6899
Eric Kudler <joeento@gmail.com>
2017-02-03 13:33:55 -0500

    Blinded city selector autocomplete to backend

--- app/scripts/restaurant-list/restaurant-list.component.js
+++ app/scripts/restaurant-list/restaurant-list.component.js
@@ -3,18 +3,21 @@
 angular
 .module('restaurantList')
 .component('restaurantList', {
-    templateUrl: 'scripts/restaurant-list/restaurant-list.template.html',
-    controller: function RestaurantListController($http) {
-        var self = this;
-
-        $http.get('api/restaurants', {params: {location: 'San Francisco'}}).then(function(response) {
-            self.restaurants = response.data;
-        });
-
-
+	templateUrl: 'scripts/restaurant-list/restaurant-list.template.html',
+	controller: function RestaurantListController($http) {
+		var self = this;
 		self.search_options = {
 			types: ['(cities)']
 		};
 
-    }
+		self.selectCity = function() {
+			$http.get('api/restaurants', {params: {location: self.city.formatted_address}}).then(function(response) {
+				self.restaurants = response.data;
+			});
+		};
+
+
+
+
+	}
 });
